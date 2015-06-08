f1119222481ddc091084b63a3e8eb9b75d6da8e9
John <johack@gmail.com>
2015-06-09 00:59:47 +0300

    slow stupid

--- index.js
+++ index.js
@@ -4,8 +4,18 @@ var app = express();
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
-app.get('/', function(request, response) {
-  response.send('Hello World!');
+app.get('/', function(request, res) {
+    var i = Math.floor((Math.random() * 3) + 1);
+    if (i == 1) {
+        res.status(200).send('In the midnight times of my life\nWhen everything seems so wrong\nJust help me to remember\nThat Your name will bring a song.!');
+    } else if (i == 2) {
+        setTimeout(function() {
+            res.status(200).send('I`m a slow and stupid server!');
+        }, 5500);
+    }
+    else {
+        res.sendStatus(500);
+    }
 });
 
 app.listen(app.get('port'), function() {
